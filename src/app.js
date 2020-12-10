dc7df8ef773822b63ef7d21407e2bee2e88c214c
PrashantGuptaa <meengineerprashant@gmail.com>
2020-12-10 20:14:14 +0530

    #1

--- src/app.js
+++ src/app.js
@@ -1,18 +1,67 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const marioModel = require('./models/marioChar');
+const marioModel = require("./models/marioChar");
+const mongoose = require("mongoose");
 
 // Middlewares
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // your code goes here
 
+const marioCharSchema = new mongoose.Schema({
+  name: String,
+  weight: Number,
+});
 
+const Mario = mongoose.model("mario", marioCharSchema);
+
+app.get("/", (req, res) => {
+  res.send({ res: "Prashant" });
+});
+
+
+app.get("/mario",(req, res) => {
+    let temp = [];
+    Mario.find().then((mario) => {
+        mario.map(eachMario => (
+            temp.push(eachMario.name)
+        ))
+    })
+    res.send(temp);
+});
+
+app.get("/mario':id", (req, res) => {
+    const id = req.params.id;
+    Mario.findById({id}).then((char) => {
+        if(!char){
+            res.status(400).send({message: "error.message"});
+        }
+        else{
+            res.send(char);
+        }
+    })
+});
+
+
+
+app.post("/mario", (req, res) => {
+  let { name, weight } = req.body;
+  if (!name || !weight) {
+    res.status(400).send({ message: "either name or weight is missing" });
+    return;
+  }
+  const mario = new Mario({
+    name,
+    weight,
+  });
+
+  mario.save().then((mario) => res.status(201).send(mario));
+});
 
 module.exports = app;
--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 //  Your code goes here
-
+const marioModel = {name:"Luigi", weight: 60}
 
 module.exports = marioModel;
